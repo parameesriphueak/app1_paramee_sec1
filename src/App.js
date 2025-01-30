ea921763d51741631d66eaa144b0885670fdade8
parameesriphueak <s6603052421038@email.kmutnb.ac.th>
2025-01-30 11:12:09 +0700

    Content2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import { userContext }  from './context'
-import Content from './context-content'
-
-
+import { userContext } from './context'
+import Header2 from './context-header2'
+import Content2 from './context-content2'
 export default function App() {
+  let [user, setUser] = React.useState('')
   return (
-    <userContext.Provider value={'Tom Jerry'}>
-      <Content/>
-      </userContext.Provider>
-  )
-}
+    < userContext.Provider value={[user, setUser]}>
+      <Header2/>
+      <Content2/>
+    </userContext.Provider>
+    )
+  }
