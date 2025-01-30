281c49622bbcb1831715364f4ae1b42a210a1d40
parameesriphueak <s6603052421038@email.kmutnb.ac.th>
2025-01-30 10:22:22 +0700

    context-content

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import React from 'react';
-import MessageBox from './state-func.js';
+import React from 'react'
+import { userContext }  from './context'
+import Content from './context-content'
 
-function App() {
-  return <MessageBox/>
+
+export default function App() {
+  return (
+    <userContext.Provider value={'Tom Jerry'}>
+      <Content/>
+      </userContext.Provider>
+  )
 }
-export default App;
